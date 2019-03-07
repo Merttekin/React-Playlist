218e4a540057b81f94dc1ad622cd8e1644063842
unknown <merttekin94@gmail.com>
2019-03-07 22:11:11 +0300

    14

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      {name: 'Mert', age: 25, belt: 'green', id: 1},
+      {name: 'Mert2', age: 30, belt: 'black', id: 2},
+      {name: 'Mert3', age: 42, belt: 'red', id: 3},
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <Ninjas name="Ryu" age="25" belt="black" />
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
