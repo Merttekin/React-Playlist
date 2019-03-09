43c0fbcb775f1549061ce1f437ac34145ba1bba5
unknown <merttekin94@gmail.com>
2019-03-09 19:00:13 +0300

    16

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
 
-class Ninjas extends Component{
-    render(){
-        const { ninjas } = this.props;
+const Ninjas = ({ninjas}) =>{
+        // const { ninjas } = props;
+        // const ninjaList = ninjas.map(ninja => {
+        //     if(ninja.age > 20){
+        //         return(
+        //             <div className="ninja" key={ninja.id}>
+        //                 <div>Name: {ninja.name}</div>
+        //                 <div>Age: {ninja.age}</div>
+        //                 <div>Belt: {ninja.belt}</div>
+        //             </div>
+        //         )
+        //     }
+        //     else{
+        //         return null;
+        //     }
+            
+        // })
         const ninjaList = ninjas.map(ninja => {
-            return(
+            return ninja.age > 20 ? (
                 <div className="ninja" key={ninja.id}>
                     <div>Name: {ninja.name}</div>
                     <div>Age: {ninja.age}</div>
                     <div>Belt: {ninja.belt}</div>
                 </div>
-            )
+            ) : null;
         })
         return(
             <div className="ninjaList">
@@ -19,6 +33,5 @@ class Ninjas extends Component{
         )
         
     }
-}
 
 export default Ninjas;
